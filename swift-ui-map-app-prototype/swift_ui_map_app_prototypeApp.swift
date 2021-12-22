07377673ec248fb42fef867fc9c34b1613fae3b0
Rick Brown <ricky.brown.00@gmail.com>
2021-12-22 22:27:31 +0000

    Initial Commit

---
+++ swift-ui-map-app-prototype/swift_ui_map_app_prototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_map_app_prototypeApp.swift
+//  swift-ui-map-app-prototype
+//
+//  Created by Rick Brown on 22/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_map_app_prototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
